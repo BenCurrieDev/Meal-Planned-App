39d61f0bb263acfb5c7536ad11cc7eafb8150162
Ben Currie <69808500+BJCurrie@users.noreply.github.com>
2022-12-19 16:52:12 +0000

    enabled locally stored state

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,24 @@
 import { configureStore } from '@reduxjs/toolkit';
 import RecipesSlice from '../features/recipes/recipesSlice';
 import IngredientsSlice from '../data/IngredientsSlice';
+import { loadState, saveState } from './localStorage';
+import throttle from 'lodash.throttle';
+
+// used as preloadedstate value to return to where user left off
+const persistedState = loadState();
 
 export const store = configureStore({
   reducer: {
     recipes: RecipesSlice,
     allIngredients: IngredientsSlice
   },
+  preloadedState: persistedState
 });
+
+
+// Updates locally stored state whenever store changes but limited to once per second
+store.subscribe(throttle(() => {
+  saveState({
+    recipes: store.getState().recipes
+  });
+}, 1000));
