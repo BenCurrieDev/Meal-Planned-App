7c2583420984efc3e2be71734a40a590a7e7dfd9
Ben Currie <69808500+BJCurrie@users.noreply.github.com>
2022-12-19 14:37:04 +0000

    added information to detailed recipe view

--- src/components/RecipeDetailedView/RecipeDetailedView.js
+++ src/components/RecipeDetailedView/RecipeDetailedView.js
@@ -1,10 +1,34 @@
 import { useSelector } from "react-redux";
 
 
-const RecipeDetailedView = (props) => {
-
+const RecipeDetailedView = ({ recipe }) => {
+    const { id, title, summary, servings, readyInMinutes, instructions, extendedIngredients } = recipe;
+    
+    
     return (
-      <h2>Data to be displayed here: {props.recipe.title}</h2>
+     <>
+       <h3>{title}</h3>
+       <button><h4>Edit</h4></button>
+       <button><h4>Delete</h4></button>
+       <h4>{summary}</h4>
+       <p>{`Serves: ${servings}`}</p>
+       <p>{`Prep & cooking time: ${readyInMinutes} minutes`}</p>
+       <h3>Instructions:</h3>
+       <p>{instructions}</p>
+       <h3>Ingredients:</h3>
+       <table>
+        {extendedIngredients.map(({ name, amount, unit }) => {
+            return (
+                <tr>
+                    <td>{name}</td>
+                    <td>{amount}</td>
+                    <td>{unit}{amount > 1 && 's'}</td>
+                </tr>
+            )
+        })}
+       </table>
+     </>
+      
     )
 }
 
