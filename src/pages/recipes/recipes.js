86abf6c4b94d112dbec6cbbc17a50317b84e5788
Ben Currie <69808500+BJCurrie@users.noreply.github.com>
2022-12-20 12:05:17 +0000

    moved component navigation buttons to separate nav

--- src/pages/recipes/recipes.js
+++ src/pages/recipes/recipes.js
@@ -4,6 +4,8 @@ import * as S from './styles';
 import { useState } from 'react';
 import EditRecipeForm from '../../components/EditRecipeForm/EditRecipeForm';
 import RecipeDetailedView from '../../components/RecipeDetailedView/RecipeDetailedView';
+import { bottomViewNavs } from './viewNavs';
+
 
 
 
@@ -13,7 +15,9 @@ const Recipes = () => {
   // SHOULD BE ABLE TO USE USEFFECT TO SET RENDERED VIEW, 
   // THIS WILL REDUCE AMOUNT OF TIMES CONDITIONALS ARE EVALAUTED
   
+
   
+
   const backToBook = () => {
     setView('displayRecipes');
   }
@@ -27,14 +31,19 @@ const Recipes = () => {
     console.log('Edit click registered');
     setView('editRecipe');
   }
+
   
   return (
     <main>
       <h2>Your Digital Recipe Book</h2>
-      {view === 'displayRecipes' && <S.ManagementContainer><RecipeDisplayArea viewDetails={viewDetails}/><button onClick={() => setView('addRecipe')}>Create Recipe</button></S.ManagementContainer>}
-      {view === 'addRecipe' && <S.ManagementContainer><NewRecipeForm backToBook={backToBook}/><button onClick={backToBook}>Back</button></S.ManagementContainer>}
-      {view === 'detailedRecipe' && <S.ManagementContainer><RecipeDetailedView recipe={activeRecipe} backToBook={backToBook} editRecipe={editRecipe}/><button onClick={backToBook}>Back</button></S.ManagementContainer>}
-      {view === 'editRecipe' && <S.ManagementContainer><EditRecipeForm recipe={activeRecipe} viewDetails={viewDetails}/><button onClick={() => viewDetails(activeRecipe)}>Cancel</button></S.ManagementContainer>}
+      {view === 'displayRecipes' && <S.ManagementContainer><RecipeDisplayArea viewDetails={viewDetails}/></S.ManagementContainer>}
+      {view === 'addRecipe' && <S.ManagementContainer><NewRecipeForm backToBook={backToBook}/></S.ManagementContainer>}
+      {view === 'browseRecipes' && <S.ManagementContainer><h3>Working on browse component</h3></S.ManagementContainer>}
+      {view === 'detailedRecipe' && <S.ManagementContainer><RecipeDetailedView recipe={activeRecipe} backToBook={backToBook} editRecipe={editRecipe}/></S.ManagementContainer>}
+      {view === 'editRecipe' && <S.ManagementContainer><EditRecipeForm recipe={activeRecipe} viewDetails={viewDetails}/></S.ManagementContainer>}
+      <nav>
+        {bottomViewNavs[view].map(({text, newView}) => <button onClick={() => setView(newView)}>{text}</button>)}
+      </nav>
     </main>
   );
 }
---
+++ src/pages/recipes/viewNavs.js
@@ -0,0 +1,20 @@
+export const bottomViewNavs = {
+    displayRecipes: [
+        {text: 'New Recipe', newView: 'addRecipe'},
+        {text: 'Browse Recipes', newView: 'browseRecipes'}
+    ],
+    addRecipe: [
+        {text: 'Cancel New Recipe', newView: 'displayRecipes'}
+    ],
+    browseRecipes: [
+        {text: 'Recipe Book', newView: 'displayRecipes'}
+    ],
+    detailedRecipe: [
+        {text: 'Back', newView: 'displayRecipes'}
+
+    ],
+    editRecipe: [
+        {text: 'Cancel Edit', newView: 'detailedRecipe'}
+
+    ]
+  }
