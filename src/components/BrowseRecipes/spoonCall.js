ca6c06a5ac32ccdd1dd620c15b68e6ce4115f031
Ben Currie <69808500+BJCurrie@users.noreply.github.com>
2022-12-21 15:18:35 +0000

    updated api call to cache recipe details

--- src/components/BrowseRecipes/spoonCall.js
+++ src/components/BrowseRecipes/spoonCall.js
@@ -1,6 +1,6 @@
 import { keys } from './config';
 import { store } from '../../app/store';
-import { cacheSearch } from '../../features/apiCalls/cachedSearchSlice';
+import { cacheSearch, cacheRecipe } from '../../features/apiCalls/cachedSearchSlice';
 
 
 const key = keys.spoonKey;
@@ -17,23 +17,17 @@ const listToString = (list, string) => {
 }
 
 export const searchRecipes = async (obj) => {
-
-    console.log('Attempting api request...')
     const searchUrl = `${endpoints.searchRecipes}?apiKey=${key}&query=${obj.query}`;
     const dietUrl = listToString(obj.diet, 'diet');
     const intolerancesUrl = listToString(obj.intolerances, 'intolerances');
     const timeUrl = obj.maxReadyTime ? `&maxReadyTime=${obj.maxReadyTime}` : '';
     const advancedSearchUrl = `${searchUrl}${dietUrl}${intolerancesUrl}${timeUrl}`;
-    console.log(advancedSearchUrl);
     try {
         const response = await fetch(advancedSearchUrl);
         if (response.ok) {
-            console.log('Request successful. Converting data before printing...')
             const result = await response.json();
-            console.log('Result: ', result);
             const toCache = {searchParams: obj, result: result}
             store.dispatch(cacheSearch(toCache));
-            console.log(store);
             return;
         }
         throw new Error(response.statusText);
@@ -48,7 +42,8 @@ export const getRecipeInformation = async (id) => {
         const response = await fetch(getInfoUrl);
         if (response.ok) {
             const result = await response.json();
-            return result;
+            store.dispatch(cacheRecipe(result));
+            return;
         }
         throw new Error(response.statusText);
     } catch (error) {
