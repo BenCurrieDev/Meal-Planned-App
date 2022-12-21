28513e18606f214fc95220b6cb1d8bdfa6cadd84
Ben Currie <69808500+BJCurrie@users.noreply.github.com>
2022-12-21 11:51:58 +0000

    made SearchParams stateless

--- src/components/BrowseRecipes/BrowseRecipes.js
+++ src/components/BrowseRecipes/BrowseRecipes.js
@@ -1,41 +1,28 @@
-import { searchRecipes } from "./spoonCall";
-import { useState } from "react";
 import { useSelector } from "react-redux";
-import { selectQuery, selectCache } from "../../features/apiCalls/cachedSearchSlice";
+import { selectCache } from "../../features/apiCalls/cachedSearchSlice";
+import { useState } from "react";
 import BrowseCard from "../BrowseCard/BrowseCard";
 import SearchParams from "../SearchParams/SearchParams";
 
 
 
 const BrowseRecipes = () => {
+    const [showOther, setShowOther] = useState(false);
     const [query, setQuery] = useState('');
     const [diet, setDiet] = useState([]);
     const [intolerances, setIntolerances] = useState([]);
     const [maxReadyTime, setMaxReadyTime] = useState(0);
-    const prevQuery = useSelector(selectQuery);
     const cache = useSelector(selectCache);
     
-
-    
-    
-    const handleSearch = () => {
-      // ensures same query not passed twice in succession to reduce unnecessary api calls
-      console.log('Query: ', query, 'Previous query: ', prevQuery);
-      if (query === prevQuery) {
-        return;
-      }
-      searchRecipes(query);
-    }
-
-
     return (
         <>
           <div>
             <SearchParams 
-            query={query} setQuery={setQuery} 
-            diet={diet} setDiet={setDiet} 
-            intolerances={intolerances} setIntolerances={setIntolerances} 
-            maxReadyTime={maxReadyTime} setMaxReadyTime={setMaxReadyTime} 
+              showOther={showOther} setShowOther={setShowOther} 
+              query={query} setQuery={setQuery} 
+              diet={diet} setDiet={setDiet} 
+              intolerances={intolerances} setIntolerances={setIntolerances} 
+              maxReadyTime={maxReadyTime} setMaxReadyTime={setMaxReadyTime} 
             />
           </div>
           <div>
--- src/components/SearchParams/SearchParams.js
+++ src/components/SearchParams/SearchParams.js
@@ -1,4 +1,6 @@
-import { useState } from "react";
+import { useSelector } from "react-redux";
+import { selectQuery } from "../../features/apiCalls/cachedSearchSlice";
+import { searchRecipes } from "../BrowseRecipes/spoonCall";
 
 
 const searchParams = {
@@ -7,17 +9,52 @@ const searchParams = {
     times: [5, 10, 15, 20, 30, 45, 60, 120]
   }
 
-const SearchParams = ({ query, setQuery, diet, setDiet, intolerances, setIntolerances, maxReadyTime, setMaxReadyTime }) => {
-    const [showOther, setShowOther] = useState(false);
+const SearchParams = ({showOther, setShowOther, query, setQuery, diet, setDiet, intolerances, setIntolerances, maxReadyTime, setMaxReadyTime }) => {
+    
+    const prevQuery = useSelector(selectQuery);
+
+    const handleSearch = (e) => {
+      // ensures same query not passed twice in succession to reduce unnecessary api calls
+      e.preventDefault();
+      if (query === prevQuery) {
+        return;
+      }
+      const paramObj = {
+        query: query,
+        diet: diet,
+        intolerances: intolerances,
+        maxReadyTime: maxReadyTime
+      }
+      searchRecipes(paramObj);
+    }
 
     const handleChange = ({ target }) => {
        setQuery(target.value);
        console.log(query);
     }
+
+    const handleIntoleranceCheck = ({ target }) => {
+       target.checked ? 
+         setIntolerances(prev => [...prev, target.value])
+         :
+         setIntolerances(() => intolerances.filter(intol => target.value !== intol));
+    }
+
+    const handleShowCheck = ({ target }) => {
+      setShowOther(target.checked);
+      console.log(target.checked);
+    }
+
+    const handleDietCheck = ({ target }) => {
+       target.checked ? 
+         setDiet(prev => [...prev, target.value])
+         :
+         setDiet(() => diet.filter(d => target.value !== d));
+    }
     
    
     return (
-       <form>
+       <form onSubmit={handleSearch}>
         <label htmlFor="recipeSearch">Search for new recipes:</label>
         <input type="search" id="recipeSearch" value={query} onChange={handleChange}/>
         <label htmlFor="time">Max Time:</label>
@@ -29,21 +66,21 @@ const SearchParams = ({ query, setQuery, diet, setDiet, intolerances, setIntoler
         
         <fieldset>
           <legend>Dietary Requirements</legend>
-          <input type="checkbox" id="vegetarian"/>
+          <input type="checkbox" id="vegetarian" value="vegetarian" onChange={handleDietCheck}/>
           <label htmlFor="vegetarian">vegetarian</label>
-          <input type="checkbox" id="vegan"/>
+          <input type="checkbox" id="vegan" value="vegan" onChange={handleDietCheck}/>
           <label htmlFor="vegan">vegan</label>
-          <input type="checkbox" id="glutenFree"/>
+          <input type="checkbox" id="glutenfree" value="gluten free" onChange={handleDietCheck}/>
           <label htmlFor="glutenFree">gluten free</label>
-          <input type="checkbox" id="other"/>
-          <label htmlFor="other">show intolerances</label>
-          {!showOther && 
+          <input type="checkbox" id="other" onChange={handleShowCheck}/>
+          <label htmlFor="other">other</label>
+          {showOther && 
               <fieldset>
-                <legend>Intolerances</legend>
+                <legend>Intolerances / Allergies</legend>
                 {searchParams.intolerances.map((intolerance, index) => {
                   return (
                     <div key={index}>
-                      <input type="checkbox" id={intolerance}/>
+                      <input type="checkbox" id={index} value={intolerance} onChange={handleIntoleranceCheck}/>
                       <label htmlFor={intolerance}>{intolerance}</label>
                     </div>
                   )
