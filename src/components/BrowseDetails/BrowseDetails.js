5357f405b27fc68547b651652a9388584313ae02
Ben Currie <69808500+BJCurrie@users.noreply.github.com>
2022-12-21 22:39:14 +0000

    changed detail view info order

--- src/components/BrowseDetails/BrowseDetails.js
+++ src/components/BrowseDetails/BrowseDetails.js
@@ -2,7 +2,7 @@
 import { useSelector } from "react-redux";
 import { selectCachedRecipes } from "../../features/apiCalls/cachedSearchSlice";
 
-const BrowseDetails =  ({ activeId }) => {
+const BrowseDetails = ({ activeId }) => {
 
     const cachedRecipes = useSelector(selectCachedRecipes);
     console.log('Logging cache: ', cachedRecipes)
@@ -11,24 +11,24 @@ const BrowseDetails =  ({ activeId }) => {
     const { extendedIngredients, image, analyzedInstructions, readyInMinutes, servings, title } = details;
     return (
         <>
-        <h3>{title}</h3>
-        <img src={image} alt={title} width="300"/>
-        <p>Serves: {servings}</p><p>Prep & cooking time: {readyInMinutes}</p>
-        <h4>Instructions</h4>
-        {analyzedInstructions[0]['steps'].map(obj => <p>{`${obj.number}. ${obj.step}`}</p>)}
-        <h4>Ingredients</h4>
-        {console.log(extendedIngredients)}
-        <table>
-        {extendedIngredients.map(({ name, amount, measures }) => {
-            return (
-                <tr>
-                    <td>{name.toLowerCase()}</td>
-                    <td>{amount.toString().length > 3 ? parseFloat(amount.toPrecision(3)) : amount}</td>
-                    <td>{measures.metric.unitShort.toLowerCase()}</td>
-                </tr>
-            )
-        })}
-       </table>
+            <h3>{title}</h3>
+            <img src={image} alt={title} width="300" />
+            <p>Serves: {servings}</p><p>Prep & cooking time: {readyInMinutes}</p>
+            <h4>Ingredients</h4>
+            {console.log(extendedIngredients)}
+            <table>
+                {extendedIngredients.map(({ name, amount, measures }) => {
+                    return (
+                        <tr>
+                            <td>{name.toLowerCase()}</td>
+                            <td>{amount.toString().length > 3 ? parseFloat(amount.toPrecision(3)) : amount}</td>
+                            <td>{measures.metric.unitShort.toLowerCase()}</td>
+                        </tr>
+                    )
+                })}
+            </table>
+            <h4>Instructions</h4>
+            {analyzedInstructions[0]['steps'].map(obj => <p>{`${obj.number}. ${obj.step}`}</p>)}
         </>
     )
 }
--- src/components/RecipeDetailedView/RecipeDetailedView.js
+++ src/components/RecipeDetailedView/RecipeDetailedView.js
@@ -12,30 +12,30 @@ const RecipeDetailedView = ({ recipe, backToBook, editRecipe }) => {
             backToBook();
         }
     }
-    
+
     return (
-     <>
-       <h3>{title}</h3>
-       <button onClick={editRecipe}><h4>Edit</h4></button>
-       <button onClick={handleDelete}><h4>Delete</h4></button>
-       <p>{`Serves: ${servings}`}</p>
-       <p>{`Prep & cooking time: ${readyInMinutes} minutes`}</p>
-       <h3>Instructions:</h3>
-       <p>{instructions}</p>
-       <h3>Ingredients:</h3>
-       <table>
-        {extendedIngredients.map(({ name, amount, unit }) => {
-            return (
-                <tr>
-                    <td>{name}</td>
-                    <td>{amount}</td>
-                    <td>{unit}{amount > 1 && 's'}</td>
-                </tr>
-            )
-        })}
-       </table>
-     </>
-      
+        <>
+            <h3>{title}</h3>
+            <button onClick={editRecipe}><h4>Edit</h4></button>
+            <button onClick={handleDelete}><h4>Delete</h4></button>
+            <p>{`Serves: ${servings}`}</p>
+            <p>{`Prep & cooking time: ${readyInMinutes} minutes`}</p>
+            <h3>Ingredients:</h3>
+            <table>
+                {extendedIngredients.map(({ name, amount, unit }) => {
+                    return (
+                        <tr>
+                            <td>{name}</td>
+                            <td>{amount}</td>
+                            <td>{unit}{amount > 1 && 's'}</td>
+                        </tr>
+                    )
+                })}
+            </table>
+            <h3>Instructions:</h3>
+            <p>{instructions}</p>
+        </>
+
     )
 }
 
