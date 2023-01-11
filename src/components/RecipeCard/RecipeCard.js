f90ad834569c18603a5d61787d11250e0ad9bdf7
Ben Currie <69808500+BJCurrie@users.noreply.github.com>
2023-01-11 15:51:44 +0000

    deleted white space

--- src/components/RecipeCard/RecipeCard.js
+++ src/components/RecipeCard/RecipeCard.js
@@ -9,7 +9,6 @@ const RecipeCard = ( props ) => {
     <li className="w-full">
         <button onClick={() => props.viewDetails(recipe)} className="rounded-md bg-gray-100 mb-2 w-full shadow-sm">
           <h4 className="text-gray-800 text-lg font-medium py-2 text-left px-2 w-full">{recipe.title}</h4>
-          
         </button>
     </li>
   )
