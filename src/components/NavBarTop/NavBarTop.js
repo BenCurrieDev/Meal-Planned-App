f05558f2440d2d6f5deb3eb33090f742836929b8
Ben Currie <69808500+BJCurrie@users.noreply.github.com>
2023-01-12 14:46:03 +0000

    added navigation functionality

--- src/components/NavBarTop/NavBarTop.js
+++ src/components/NavBarTop/NavBarTop.js
@@ -1,19 +1,47 @@
-// import ROUTES from "../../app/routes";
-// import { NavLink } from "react-router-dom";
+import ROUTES from "../../app/routes";
+import { NavLink } from "react-router-dom";
 
-// let isMealPlan = false;  // replace with a state variable
-// let currentDayView = 'Today';  // replace with a state variable
+let test = false;
+
+
+
+
+
 
 const NavBarTop = () => {
 	return (
-		<div className="flex justify-between bg-gray-700 h-14 fixed z-20 bottom-0 w-screen">
-			<img src="./Logo-White-117x35.png" alt="logo" className="py-4 pl-6" />
-			<span className="material-symbols-outlined text-white flex items-center text-2xl pr-6">
-				menu
-			</span>
-		</div>
+			<div className="flex justify-between bg-gray-700 h-14 fixed z-20 bottom-0 w-screen">
+				<img src="./Logo-White-117x35.png" alt="logo" className="py-4 pl-6" />
+				<ul className="flex items-center">
+					<li>
+						<NavLink to={ROUTES.homeRoute()}>
+							<span className="material-symbols-outlined text-white flex items-center text-2xl ml-10">
+								home
+							</span>
+						</NavLink>
+					</li>
+					<li>
+						<NavLink to={ROUTES.recipesRoute()}>
+							<span className="material-symbols-outlined text-white flex items-center text-2xl ml-10">
+								menu_book
+							</span>
+						</NavLink>
+					</li>
+					<li>
+						<NavLink to={ROUTES.plannerRoute()}>
+							<span className="material-symbols-outlined text-white flex items-center text-2xl mr-10 ml-10">
+								calendar_month
+							</span>
+						</NavLink>
+					</li>
+				</ul>
+				
+				
+			</div>
 	)
 }
+
+
 
 
 // Original desktop first code
