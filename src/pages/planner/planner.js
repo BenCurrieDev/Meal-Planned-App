8c8a58445b1df819ece3de8348a2e88c66343beb
Ben Currie <69808500+BJCurrie@users.noreply.github.com>
2023-01-19 19:16:24 +0000

    added navigation to and from create plan

--- src/pages/planner/planner.js
+++ src/pages/planner/planner.js
@@ -1,12 +1,19 @@
-
+import { useState } from "react";
+import { bottomViewNavs } from "./viewNavs";
 
 
 const Planner = () => {
+    const [ view, setView ] = useState('displayPlans');
+
     return (
         <main className='min-h-full bg-gray-200'>
              <div className=" bg-gray-700">
                 <h2 className="text-white text-3xl font-normal text-center py-6">Meal Planner</h2>
              </div>
+
+             <nav className='flex justify-center'>
+                {bottomViewNavs[view].map(({text, newView}, index) => <button key={index} onClick={() => setView(newView)} className="rounded-2xl bg-gray-700 text-gray-200 py-2 px-4 m-2 text-xl font-light mb-20">{text}</button>)}
+            </nav>
         </main>
     )
 }
---
+++ src/pages/planner/viewNavs.js
@@ -0,0 +1,8 @@
+export const bottomViewNavs = {
+    displayPlans: [
+        {text: 'Create Plan', newView: 'createPlan'}
+    ],
+    createPlan: [
+        {text: 'Back', newView: 'displayPlans'}
+    ]
+  }
