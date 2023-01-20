250dd9cb8cc517fdbd5fb95ddf2c2910b8bd21a2
Ben Currie <69808500+BJCurrie@users.noreply.github.com>
2023-01-20 06:47:31 +0000

    created CreateMealPlan component

--- src/pages/planner/planner.js
+++ src/pages/planner/planner.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { bottomViewNavs } from "./viewNavs";
+import CreateMealPlan from "../../components/CreateMealPlan/CreateMealPlan";
 
 
 const Planner = () => {
@@ -10,7 +11,8 @@ const Planner = () => {
              <div className=" bg-gray-700">
                 <h2 className="text-white text-3xl font-normal text-center py-6">Meal Planner</h2>
              </div>
-
+             {view === 'displayPlans' && <div></div>}
+             {view === 'createPlan' && <div><CreateMealPlan /></div>}
              <nav className='flex justify-center'>
                 {bottomViewNavs[view].map(({text, newView}, index) => <button key={index} onClick={() => setView(newView)} className="rounded-2xl bg-gray-700 text-gray-200 py-2 px-4 m-2 text-xl font-light mb-20">{text}</button>)}
             </nav>
